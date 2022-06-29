1d9ee0367ed0ed3c4065ad3772d8af3934291f1a
big-meel <big.meel@zoho.com>
2022-06-28 22:36:15 -0500

    Initial commit

---
+++ src/main/java/com/example/gfnlibraryv2/GfnLibraryV2Application.java
@@ -0,0 +1,13 @@
+package com.example.gfnlibraryv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfnLibraryV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfnLibraryV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gfnlibraryv2/GfnLibraryV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gfnlibraryv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfnLibraryV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
