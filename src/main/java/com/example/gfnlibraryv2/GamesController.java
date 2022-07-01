57ece15a8f9801d6157eba0117f7ba801f0d7ed9
big-meel <big.meel@zoho.com>
2022-07-01 02:51:46 -0500

    Set up index to show all games in games controller

--- src/main/java/com/example/gfnlibraryv2/GamesController.java
+++ src/main/java/com/example/gfnlibraryv2/GamesController.java
@@ -1,14 +1,21 @@
 package com.example.gfnlibraryv2;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class GamesController {
 
+    @Autowired
+    private GameRepository repo;
+
     @GetMapping("/")
     public String index(){
-        return "Games will be listed here";
+        for (Game game : repo.findAll()) {
+            return String.format("%s", game.toString());
+        }
+        return "End";
     }
      
 }
